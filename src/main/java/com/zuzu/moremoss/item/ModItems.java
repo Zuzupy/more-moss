77ffe19fc41314d36f597074da7380b88e7dc1f3
Zuzupy <zuzudev.py@gmail.com>
2024-05-23 23:30:22 -0400

    Add dispenser functionality, rework `MossBall.useOnBlock()` to use a helper class (shared code between it and `MossBall.dispenserUseOnBlock()`)

--- src/main/java/com/zuzu/moremoss/item/ModItems.java
+++ src/main/java/com/zuzu/moremoss/item/ModItems.java
@@ -4,6 +4,7 @@ import com.zuzu.moremoss.MoreMoss;
 import com.zuzu.moremoss.moss_ball.MossBall;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.block.DispenserBlock;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
@@ -23,6 +24,14 @@ public class ModItems {
 
     public static void registerModItems() {
         MoreMoss.LOGGER.info("Registering ModItems for " + MoreMoss.MOD_ID);
+
+        DispenserBlock.registerBehavior(MOSS_BALL, (pointer, stack) -> {
+            MOSS_BALL.dispenserUseOnBlock(pointer);
+            stack.decrement(1);
+
+            return stack;
+        });
+
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
     }
 }
--- src/main/java/com/zuzu/moremoss/moss_ball/MossBall.java
+++ src/main/java/com/zuzu/moremoss/moss_ball/MossBall.java
@@ -1,13 +1,16 @@
 package com.zuzu.moremoss.moss_ball;
 
-import com.zuzu.moremoss.MoreMoss;
 import com.zuzu.moremoss.config_loader.ConfigLoader;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPointer;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 import java.util.Objects;
@@ -22,20 +25,36 @@ public class MossBall extends Item {
     public ActionResult useOnBlock(ItemUsageContext context) {
         World world = context.getWorld();
         Block block = world.getBlockState(context.getBlockPos()).getBlock();
+
+        if (useItem(world, block, context.getBlockPos())) {
+            Objects.requireNonNull(context.getPlayer()).getStackInHand(context.getHand()).decrement(1);
+            return ActionResult.SUCCESS;
+        }
+
+        return ActionResult.FAIL;
+    }
+
+    public void dispenserUseOnBlock(BlockPointer pointer) {
+        World world = pointer.world();
+        BlockPos pos = pointer.pos().offset(world.getBlockState(pointer.pos()).get(Properties.FACING));
+        Block block = world.getBlockState(pos).getBlock();
+
+        useItem(world, block, pos);
+    }
+
+    private static boolean useItem(World world, Block block, BlockPos pos) {
         Block convertTo = ConfigLoader.getConversion(block);
 
         if (convertTo == null) {
-            return ActionResult.FAIL;
+            return false;
         }
 
-        if (world.isClient()) {
-            Objects.requireNonNull(context.getPlayer()).playSound(SoundEvents.ITEM_BONE_MEAL_USE, 1.0f, 1.0f);
-            MoreMoss.LOGGER.info("{} -> {}", block.toString(), convertTo);
-        } else {
-            world.setBlockState(context.getBlockPos(), convertTo.getStateWithProperties(world.getBlockState(context.getBlockPos())));
-            Objects.requireNonNull(context.getPlayer()).getStackInHand(context.getHand()).decrement(1);
+        world.playSoundAtBlockCenter(pos, SoundEvents.ITEM_BONE_MEAL_USE, SoundCategory.BLOCKS, 1.0f, 1.0f, true);
+
+        if (!(world.isClient())) {
+            world.setBlockState(pos, convertTo.getStateWithProperties(world.getBlockState(pos)));
         }
 
-        return ActionResult.SUCCESS;
+        return true;
     }
 }
