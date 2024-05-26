2f41ed7078ad2aeaa4ac34386d68a420c2c2d096
Zuzupy <zuzudev.py@gmail.com>
2024-05-25 23:42:35 -0400

    Use dataGen for crafting

--- src/main/java/com/zuzu/moremoss/datagen/ModRecipeProvider.java
+++ src/main/java/com/zuzu/moremoss/datagen/ModRecipeProvider.java
@@ -1,8 +1,12 @@
 package com.zuzu.moremoss.datagen;
 
+import com.zuzu.moremoss.block.ModBlocks;
+import com.zuzu.moremoss.item.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.recipe.book.RecipeCategory;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
     public ModRecipeProvider(FabricDataOutput output) {
@@ -11,6 +15,6 @@ public class ModRecipeProvider extends FabricRecipeProvider {
 
     @Override
     public void generate(RecipeExporter exporter) {
-
+        offerReversibleCompactingRecipes(exporter, RecipeCategory.BUILDING_BLOCKS, ModItems.MOSS_BALL, RecipeCategory.MISC, Blocks.MOSS_BLOCK);
     }
 }
